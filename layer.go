39495d61cc9659bbcaf3007bcaeb6a876401cf8b
Philipp Klose <TheHippo@users.noreply.github.com>
2022-12-05 23:01:35 +0000

    added documentation to layer.go

--- layer.go
+++ layer.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// Layer represents a caching layer
 type Layer struct {
 	cache     Cacher
 	evictions []EvictionStrategy
@@ -17,19 +18,28 @@ type Layer struct {
 	lock      sync.RWMutex
 }
 
+// Item within the caching layer
+// TODO: this does not need to be exported
 type Item struct {
 	name       string
 	lastAccess time.Time
 	size       int64
 }
 
+// LayerStats contains information about the cache items
+// in the layer
+// * Count of items
+// * Size of items in bytes
 type LayerStats struct {
 	Count int32
 	Size  int64
 }
 
+// compile time check
 var _ Cacher = &Layer{}
 
+// NewLayer creates a new caching layer with various eviction strategies.
+// If no evicition strategy is passed the items will never be deleted.
 func NewLayer(cache Cacher, evictions ...EvictionStrategy) *Layer {
 	return &Layer{
 		cache:     cache,
@@ -39,6 +49,9 @@ func NewLayer(cache Cacher, evictions ...EvictionStrategy) *Layer {
 	}
 }
 
+// BackgroundEviction enabled eviction of stale items in the background.
+// Items are checked for eviction according to dur. This function blocks
+// and stop it provide a Context that can be canceled.
 func (l *Layer) BackgroundEviction(ctx context.Context, dur time.Duration) {
 	ticker := time.NewTicker(dur)
 	for {
@@ -81,6 +94,8 @@ func (l *Layer) evict() {
 	}
 }
 
+// Delete an items from the underlying cache. The exact error
+// depends on the underlying cache implementation.
 func (l *Layer) Delete(name string) error {
 	if err := l.cache.Delete(name); err != nil {
 		return err
@@ -100,6 +115,8 @@ func (l *Layer) Delete(name string) error {
 	return nil
 }
 
+// Exists checks if an items exists in the underlying cache.
+// Does not count as an access.
 func (l *Layer) Exists(name string) bool {
 	return l.cache.Exists(name)
 }
@@ -124,6 +141,10 @@ func (l *Layer) accessed(name string, size int64) {
 	l.lock.Unlock()
 }
 
+// Get an item from the layer. Returns the content
+// or an error if something went wrong. The behavior of
+// the error depends on the underlying cache. This also
+// counts as an access to the item.
 func (l *Layer) Get(name string) ([]byte, error) {
 	content, err := l.cache.Get(name)
 	if err != nil {
@@ -133,6 +154,10 @@ func (l *Layer) Get(name string) ([]byte, error) {
 	return content, nil
 }
 
+// Put an item into the layer. Required a key and the content itself.
+// Returns an error if something went wrong. The exact error depends
+// on the underlying cache. If the item already exists the item is
+// overwritten. This also counts as an access to the item.
 func (l *Layer) Put(name string, content []byte) error {
 	if err := l.cache.Put(name, content); err != nil {
 		return err
@@ -155,6 +180,7 @@ func (l *Layer) Put(name string, content []byte) error {
 	return nil
 }
 
+// Stats returns the current state of the layer.
 func (l *Layer) Stats() *LayerStats {
 	return &LayerStats{
 		Count: l.count.Load(),
